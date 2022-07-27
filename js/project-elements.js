7f60ffcea114dc7ad6244216f2b3840ddf2d42e6
TonyMckes <tonymckes@gmail.com>
2022-07-27 15:00:46 -0400

    Added assets from Github

--- js/project-elements.js
+++ js/project-elements.js
@@ -9,7 +9,10 @@ function project({ description, homepageUrl, repositoryTopics, name, url }) {
   <li class="projects__item project">
       <h3 class="project__title title">${title}</h3>
       <video class="project__media" loop muted>
-        <source src="images/bg/${name}.webm" type="video/webm">
+        <source
+          src="https://raw.githubusercontent.com/TonyMckes/${name}/assets/${name}.webm"
+          type="video/webm"
+        />
         Your browser does not support the videos.
       </video>
       <div class="project__description">
