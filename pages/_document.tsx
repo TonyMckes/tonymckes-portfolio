c873fb582e74a1dbf63a98c9bc0e25f92e42b833
TonyMckes <tonymckes@gmail.com>
2022-09-12 15:22:43 -0400

    add: custom document

---
+++ pages/_document.tsx
@@ -0,0 +1,38 @@
+import { Head, Html, Main, NextScript } from "next/document";
+
+function Document() {
+
+  return (
+    <Html>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="description" content="Portfolio's" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#94a3b8" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+export default Document;
