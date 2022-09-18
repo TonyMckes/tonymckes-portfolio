086d339e3b336c881015b4f50725979a1f161d2c
TonyMckes <tonymckes@gmail.com>
2022-09-18 19:06:36 -0400

    refactor: added rel and target props

--- components/Navbar/NavItem.tsx
+++ components/Navbar/NavItem.tsx
@@ -1,6 +1,6 @@
 import Icon, { IconsCollection } from "components/Icon";
 import Link from "next/link";
-import { MouseEvent, ReactNode } from "react";
+import { HTMLAttributeAnchorTarget, MouseEvent, ReactNode } from "react";
 import { Url } from "url";
 import styles from "./NavItem.module.css";
 
@@ -13,6 +13,8 @@ export interface AnchorProps extends BaseProps {
   as?: "a";
   href: Url | string;
   text?: string;
+  target?: HTMLAttributeAnchorTarget;
+  rel?: string;
 }
 
 export interface ButtonProps extends BaseProps {
@@ -20,6 +22,8 @@ export interface ButtonProps extends BaseProps {
   href?: never;
   text: string;
   onClick?: (event: MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => void;
+  target?: never;
+  rel?: never;
 }
 type Props = AnchorProps | ButtonProps;
 
@@ -31,12 +35,19 @@ function NavItem({
   name,
   text,
   className,
+  rel,
+  target,
   ...props
 }: Props) {
   const AsComponent = as || "a";
 
   const NavItemComponent = (
-    <AsComponent className={`${styles.navLink} ${className || ""}`} {...props}>
+    <AsComponent
+      className={`${styles.navLink} ${className || ""}`}
+      rel={rel}
+      target={target}
+      {...props}
+    >
       {icon && name && <Icon name={name} />}
       {children}
       {<span className={icon ? "sr-only" : ""}>{name || text}</span>}
@@ -49,5 +60,4 @@ function NavItem({
 
   return <Link href={href}>{NavItemComponent}</Link>;
 }
-
 export default NavItem;
