dcb71498a0e512eef2c45e3510bff4c8d9addb54
TonyMckes <tonymckes@gmail.com>
2022-09-18 22:50:46 -0400

    add: script for initial theme color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import { Head, Html, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 function Document() {
-
   return (
     <Html>
       <Head>
@@ -30,6 +30,17 @@ function Document() {
       <body>
         <Main />
         <NextScript />
+        <Script id="theme-initialize" strategy="beforeInteractive">
+          {`
+          const THEME_COLOR = localStorage.getItem("theme-color");
+          const OS_COLOR_SCHEME = window.matchMedia("(prefers-color-scheme: dark)");
+          
+
+          if (THEME_COLOR === "dark" || (!THEME_COLOR && OS_COLOR_SCHEME.matches)) {
+            document.documentElement.dataset.theme = "dark";
+          }
+          `}
+        </Script>
       </body>
     </Html>
   );
