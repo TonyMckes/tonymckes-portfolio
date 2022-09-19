3ce4569127e4b390655158bff7c701462211b20d
TonyMckes <tonymckes@gmail.com>
2022-09-18 22:41:46 -0400

    refactor: added layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import "../styles/globals.css";
+import Layout from "components/Layout";
 import type { AppProps } from "next/app";
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Portfolio</title>
+      </Head>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
