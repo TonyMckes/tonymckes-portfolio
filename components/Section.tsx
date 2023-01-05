d9bfc9bf3f202da82a2b5118e6fe9e4217d9c9b9
TonyMckes <tonymckes@gmail.com>
2023-01-05 07:16:26 -0400

    fix: section subheadings

--- components/Section.tsx
+++ components/Section.tsx
@@ -17,11 +17,11 @@ function Section({ id, children }: Pick<SectionProps, "id" | "children">) {
 }
 
 export function SectionTitle({ text }: Pick<SectionProps, "text">) {
-  return <h1 className={styles.title}>{text}</h1>;
+  return <h2 className={styles.title}>{text}</h2>;
 }
 
 export function SectionSubtitle({ children }: Pick<SectionProps, "children">) {
-  return <h2 className={styles.subtitle}>{children}</h2>;
+  return <h3 className={styles.subtitle}>{children}</h3>;
 }
 
 Section.Title = SectionTitle;
