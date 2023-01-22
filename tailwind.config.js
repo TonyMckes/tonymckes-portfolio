2c1f6ed363335a3c639b7bf7df85d030f316c588
TonyMckes <tonymckes@gmail.com>
2023-01-22 11:38:30 -0400

    feat: add color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,4 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -10,7 +10,20 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["var(--font-open-sans)", ...defaultTheme.fontFamily.serif],
+        sans: ['var(--font-open-sans)', ...defaultTheme.fontFamily.serif],
+      },
+      colors: {
+        primary: {
+          100: '#e2e6e9',
+          200: '#c6ccd3',
+          300: '#a9b3bc',
+          400: '#8d99a6',
+          500: '#708090',
+          600: '#5a6673',
+          700: '#434d56',
+          800: '#2d333a',
+          900: '#161a1d',
+        },
       },
     },
   },
