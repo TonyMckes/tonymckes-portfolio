13ae592053f8c9c41e2e8296071cded01262339d
TonyMckes <tonymckes@gmail.com>
2023-01-26 20:36:26 -0400

    refactor: migrate meta tags

--- app/head.tsx
+++ app/head.tsx
@@ -3,7 +3,39 @@ export default function Head() {
     <>
       <title>Anthony Mackensen</title>
       <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta charSet="UTF-8" />
+      <meta name="description" content="Anthony portfolio" />
+      <meta
+        name="theme-color"
+        media="(prefers-color-scheme: light)"
+        content="#f5f5f5"
+      />
+      <meta
+        name="theme-color"
+        media="(prefers-color-scheme: dark)"
+        content="#0f1116"
+      />
+
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
       <link rel="icon" href="/favicon.ico" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#94a3b8" />
+      <link rel="manifest" href="manifest.json" />
     </>
   );
 }
