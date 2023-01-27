de872b16fb4bb2ee33463be31fe7b801b218d200
TonyMckes <tonymckes@gmail.com>
2023-01-27 12:33:02 -0400

    fix: anchor types

--- types/personal-info-types.ts
+++ types/personal-info-types.ts
@@ -1,10 +1,14 @@
-import { AnchorProps } from "components/Navbar/NavItem";
+import { AnchorHTMLAttributes } from "react";
 
 export type PersonalEmail = string;
+interface AnchorProps extends AnchorHTMLAttributes<HTMLAnchorElement> {
+  name: string;
+  text: string;
+}
 
 export interface PersonalInfoTypes {
   email: PersonalEmail;
-  socialMedia: Pick<Required<AnchorProps>, "name" | "href">[];
+  socialMedia: Pick<AnchorProps, "name" | "href">[];
   navLinks: Pick<AnchorProps, "text" | "href">[];
   skills: string[];
 }
