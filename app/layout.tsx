14ee69fe5b657b4be097502aa1da8fb23c6091cd
Anthony Mackensen <tonymckes@gmail.com>
2023-03-14 19:05:07 -0400

    refactor: add nextjs metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,27 @@
-import { Open_Sans } from "@next/font/google";
+import { Open_Sans } from "next/font/google";
 import Footer from "components/Footer";
 import Navbar from "components/Navbar/Navbar";
 import ThemeLoader from "components/ThemeLoader";
 import "./globals.css";
+
+export const metadata = {
+  title: "Anthony Mackensen",
+  description: "Anthony Mackensen’s portfolio",
+  manifest: "manifest.json",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "#f5f5f5" },
+    { media: "(prefers-color-scheme: dark)", color: "#0f1116" },
+  ],
+  icons: {
+    apple: "/apple-touch-icon.png",
+    icon: [
+      "/favicon.ico",
+      { type: "image/png", sizes: "16x16", url: "/favicon-16x16.png" },
+      { type: "image/png", sizes: "32x32", url: "/favicon-32x32.png" },
+    ],
+    other: { rel: "mask-icon", url: "/safari-pinned-tab.svg" },
+  },
+};
 
 const openSans = Open_Sans({
   display: "optional",
