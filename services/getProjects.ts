8f043b02621dff5de7283e2e3b55bc89fe3424b7
Anthony Mackensen <tonymckes@gmail.com>
2023-04-06 12:38:08 -0400

    Revert "feat: add revalidation ISR"
    This reverts commit 0a43a89e6dae4e59bd4d5db7696839f61179e52d.

--- services/getProjects.ts
+++ services/getProjects.ts
@@ -24,7 +24,6 @@ export const getFeaturedRepos = async (): Promise<Repository[]> => {
     const response = await fetch(API_URL, {
       ...OPTIONS,
       body: FEATURED_REPOS_QUERY,
-      next: { revalidate: 5 },
     })
 
     if (!response.ok) throw { message: 'Fetch error' }
@@ -46,7 +45,6 @@ export const getShowcaseRepos = async (): Promise<Repository[]> => {
     const response = await fetch(API_URL, {
       ...OPTIONS,
       body: SHOWCASE_REPOS_QUERY,
-      next: { revalidate: 5 },
     })
 
     if (!response.ok) throw { message: 'Fetch error' }
