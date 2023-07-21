d19cf544b5348b22ac8dd19adcb126a7f8b463f1
Anthony Mackensen <tonymckes@gmail.com>
2023-07-21 10:46:09 -0400

    refactor: add text indicator on mobile menu

--- components/Navbar/TopHeaderBar.tsx
+++ components/Navbar/TopHeaderBar.tsx
@@ -49,7 +49,7 @@ function TopHeaderBar({ children }: { children: ReactNode }) {
   const headerStyles = showNavbar
     ? 'shadow-none -translate-y-full'
     : isScrolled
-    ? 'shadow-md dark:shadow-night-700 backdrop-blur'
+    ? 'shadow-md dark:shadow-night-900 backdrop-blur'
     : 'md:py-4 duration-500 bg-transparent dark:bg-transparent'
 
   return (
@@ -69,6 +69,7 @@ function TopHeaderBar({ children }: { children: ReactNode }) {
             <TonyMckes />
           </a>
         </nav>
+
         <div className="flex">
           <button
             type="button"
@@ -82,6 +83,7 @@ function TopHeaderBar({ children }: { children: ReactNode }) {
             />
           </button>
 
+          {/* Blurred Background */}
           <div
             onClick={handleClick}
             className={tw(
@@ -97,11 +99,12 @@ function TopHeaderBar({ children }: { children: ReactNode }) {
             )}
           />
 
+          {/* Mobile Nav Menu */}
           <div
             className={tw(
               'bg-neutral-200 dark:bg-night-800 md:bg-transparent dark:md:bg-transparent',
-              'border border-black/25 dark:border-white/25 md:border-none',
-              'divide-y divide-black/25 dark:divide-white/25 md:divide-y-0',
+              'border border-black/10 dark:border-white/20 md:border-none',
+              'divide-y divide-black/10 dark:divide-white/20 md:divide-y-0',
               'fixed md:static',
               'md:divide-x',
               'md:flex',
@@ -109,7 +112,7 @@ function TopHeaderBar({ children }: { children: ReactNode }) {
               'right-6 top-14',
               'rounded-2xl',
               'shadow-lg md:shadow-none',
-              'space-y-4 md:space-y-0',
+              'space-y-2 md:space-y-0',
               'w-3/5 md:w-auto',
               menuOpen ? '' : 'invisible md:visible'
             )}
@@ -117,7 +120,9 @@ function TopHeaderBar({ children }: { children: ReactNode }) {
             {/* Navigation links */}
             {children}
 
-            <ThemeToggler />
+            <div className="before:mx-2 before:text-sm before:text-night-500 before:content-['Dark/Light_Toggler'] before:dark:text-night-400 md:px-2 md:before:hidden">
+              <ThemeToggler />
+            </div>
           </div>
         </div>
       </div>
--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -14,7 +14,7 @@ function Navbar() {
   return (
     <TopHeaderBar>
       <nav>
-        <ul className="h-full auto-cols-fr grid-flow-col space-y-2 md:mr-2 md:grid md:space-y-0">
+        <ul className="h-full auto-cols-fr grid-flow-col space-y-1 before:mx-2 before:text-sm before:text-night-500 before:content-['Navigate_to'] before:dark:text-night-400 md:grid md:space-y-0 md:px-2 md:before:hidden">
           {navLinks.map(({ href, text }) => (
             <li key={text}>
               <a
@@ -31,21 +31,23 @@ function Navbar() {
         </ul>
       </nav>
 
-      <ul className="mt-4 flex h-full md:ml-2 md:mt-0">
-        {socials.map(({ href, name }) => (
-          <li key={name}>
-            <a
-              className="nav-item underline-effect"
-              rel="noreferrer"
-              target="_blank"
-              href={href}
-            >
-              <Logo height={28} icon={name} />
-              <span className="sr-only">{name}</span>
-            </a>
-          </li>
-        ))}
-      </ul>
+      <div className="before:mx-2 before:text-sm before:text-night-500 before:content-['Socials'] before:dark:text-night-400 md:px-2 md:before:hidden">
+        <ul className="flex h-full">
+          {socials.map(({ href, name }) => (
+            <li key={name}>
+              <a
+                className="nav-item underline-effect"
+                rel="noreferrer"
+                target="_blank"
+                href={href}
+              >
+                <Logo height={28} icon={name} />
+                <span className="sr-only">{name}</span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </TopHeaderBar>
   )
 }
